21ad5c3d161ba7d93afbc173bf0ace1803b2cb51
melketsedek <melketsedekgettnet@gmail.com>
2026-09-17 09:32:27 +0300

    style(toast): quiet toast cards, fixed light theme, bottom-right

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         {children}
-        <Toaster position="top-center" closeButton />
+        <Toaster position="bottom-right" closeButton />
       </body>
     </html>
   );
